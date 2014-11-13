8bc0579fcb818697a8c77f05838dc2f945c2dd43
Sahar Massachi <sayhar@gmail.com>
2014-11-13 16:44:53 -0500

    Fixed stupid import error

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 * Import pip3 requirements from requirements.txt
 * Python3
-* When you run py.test, the functions from simple.py aren't being imported correctly. Why?
--- test_simple.py
+++ test_simple.py
@@ -1,4 +1,4 @@
-import simple
+from simple import *
 
 def two():
     return 2
@@ -7,7 +7,7 @@ def test_one():
     assert one() == 1
 
 def test_two():
-    assert two() == 2
+    assert two() == 9, "it's okay, I expect this to fail"
 
 def test_sample():
     assert return_sample()['Sergey Brin'] == "s.brin@google.com"    
